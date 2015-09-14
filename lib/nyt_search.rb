14e5ba0f1a0991e84c320e6d84406c0d8b1077b9
Vitaliy <croaton6@gmail.com>
2015-09-14 04:50:21 +0300

    add minimal json response

--- lib/nyt_search.rb
+++ lib/nyt_search.rb
@@ -1,4 +1,6 @@
 require "nyt_search/version"
+require "nyt_search/client"
+require "httparty"
 
 module NytSearch
   # Your code goes here...
--- lib/nyt_search/client.rb
+++ lib/nyt_search/client.rb
@@ -3,5 +3,11 @@ module NytSearch
     def initialize(access_token = nil)
       @access_token = access_token || ENV["NYT_ACCESS_TOKEN"]
     end
+
+    def perform_request(query)
+      url = "http://api.nytimes.com/svc/search/v2/articlesearch.json?q=#{query}&api-key=#{@access_token}"
+      response = HTTParty.get(url)
+      JSON.parse(response.body)
+    end
   end
 end

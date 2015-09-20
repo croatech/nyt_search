eea9718226ccb1666770de25886e7af90fbe1257
Vitaliy <croaton6@gmail.com>
2015-09-21 02:58:41 +0300

    del self from methods

--- lib/nyt_search/api.rb
+++ lib/nyt_search/api.rb
@@ -2,18 +2,20 @@ module NytSearch
   class Api
     include HTTParty
 
-    def self.config
-      @@config ||= {}
-    end
+    class << self
+      def config
+        @@config ||= {}
+      end
 
-    def self.key(api_key)
-      self.config[:api_key] = api_key
-      self.config[:base_uri] = "http://api.nytimes.com/svc/search/v2/articlesearch.json"
-    end
+      def key(api_key)
+        self.config[:api_key] = api_key
+        self.config[:base_uri] = "http://api.nytimes.com/svc/search/v2/articlesearch.json"
+      end
 
-    def self.get_json(url)
-      response = HTTParty.get(url)
-      JSON.parse(response.body)
+      def get_json(url)
+        response = HTTParty.get(url)
+        JSON.parse(response.body)
+      end
     end
   end
 end
